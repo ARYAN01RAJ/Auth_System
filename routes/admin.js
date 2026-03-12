530b399f4163332597ca214b59f66b309c157d35
Aryan Raj <aryanrajpanki@gmail.com>
2026-03-12 23:14:46 +0530

    completed email verification, signup, and login

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const Router = require("express");
 const router = Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { Admin } = require("../db");
 const jwtPassword = process.env.JWT_SECRET;
 const { sendEmail } = require("../services/emailService");
@@ -11,18 +12,41 @@ router.post("/signup", async (req,res)=>{
   const { email, password } = req.body;
   const hash = await bcrypt.hash(password,10);
   const verificationToken = crypto.randomBytes(32).toString("hex");
-  const user = new Admin({
+  const link = `http://localhost:3000/admin/verify-email?token=${verificationToken}`;
+  const admin = new Admin({
     email,
     password: hash,
     verificationToken,
     isVerified: false
   });
   
-  await user.save();
-  res.json({
+  await admin.save();
+  await sendEmail(
+    email,
+    "verify-email",
+    `Click this link to verify account ${link}`
+  );
+  res.status(200).json({
     msg: "Signup successful. Check your email."
   });
 });
+
+router.get("/verify-email", async (req, res)=>{
+  const verificationToken = req.query.token;
+  const admin = await Admin.findOne({verificationToken: verificationToken});
+  if(admin){
+    admin.isVerified = true
+    admin.verificationToken = null
+    await admin.save();
+    res.status(200).json({
+      msg: "Account verified successfully"
+    });
+  }else{
+    res.status(400).json({
+      msg: "Invalid or expired token"
+    })
+  }
+})
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -42,9 +66,9 @@ router.post("/login", async (req, res) => {
   const token = jwt.sign(
     { email: email },
     jwtPassword,
-    { expiresIn: "1h" }
+    { expiresIn: "1m" }
   );
-  res.json({ token });
+  res.status(200).json({ token });
 
 });
 
